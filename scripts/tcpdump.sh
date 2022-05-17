890859c321c23b9f7e523bf29b7db926449bd3c3
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2022-05-17 14:52:35 -0700

    Add -Z root to tcpdump.sh
    Signed-off-by: Kevin Grigorenko <kevin.grigorenko@us.ibm.com>

--- scripts/tcpdump.sh
+++ scripts/tcpdump.sh
@@ -1,28 +1,53 @@
 #!/bin/sh
 usage() {
-  printf "Usage: %s [-v] [-d DURATION]\n" "$(basename "${0}")"
+  printf "Usage: %s [-0 DURATION] [tcpdump arguments...]\n" "$(basename "${0}")"
   cat <<"EOF"
-             -d: DURATION (in seconds) for the collection
-             -v: verbose output to stderr
+             -0: DURATION (in seconds) for the collection. Default 60
+              *: All other options from tcpdump. Overridden defaults:
+                 -B 4096
+                 -C 100
+                 -i any
+                 -nn
+                 -s 80
+                 -v
+                 -w diag_capture_$(hostname)_$(date +%Y%m%d_%H%M%S).pcap
+                 -W 10
+                 -Z root
 EOF
   exit 2
 }
 
-VERBOSE=""
 DURATION=60
 INTERFACE="any"
+SNAPLEN=80
+BUFFERSIZE=4096
+FILESIZE=100
+FILECOUNT=10
 
+OPTERR=0
 OPTIND=1
-while getopts "d:hv?" opt; do
+while getopts "0:B:C:hi:s:W:?" opt; do
   case "$opt" in
-    d)
+    0)
       DURATION="${OPTARG}"
+      ;;
+    B)
+      BUFFERSIZE="${OPTARG}"
+      ;;
+    C)
+      FILESIZE="${OPTARG}"
       ;;
     h|\?)
       usage
       ;;
-    v)
-      VERBOSE="-v"
+    i)
+      INTERFACE="${OPTARG}"
+      ;;
+    s)
+      SNAPLEN="${OPTARG}"
+      ;;
+    W)
+      FILECOUNT="${OPTARG}"
       ;;
   esac
 done
@@ -33,4 +58,4 @@ if [ "${1:-}" = "--" ]; then
   shift
 fi
 
-run.sh sh -c "echo \"[\$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] tcpdump.sh: Starting tcpdump with duration ${DURATION}s and interface ${INTERFACE}\" && timeout ${DURATION} tcpdump -nn -v -i ${INTERFACE} -B 4096 -s 80 -C 100 -W 10 -Z root -w diag_capture_\$(hostname)_\$(date +%Y%m%d_%H%M%S).pcap &> stdouterr.txt"
+run.sh sh -c "echo \"[\$(date '+%Y-%m-%d %H:%M:%S.%N %Z')] tcpdump.sh: Starting tcpdump with duration ${DURATION}s and interface ${INTERFACE}\" && timeout ${DURATION} tcpdump -nn -v -i ${INTERFACE} -B ${BUFFERSIZE} -s ${SNAPLEN} -C ${FILESIZE} -W ${FILECOUNT} -w diag_capture_\$(hostname)_\$(date +%Y%m%d_%H%M%S).pcap -Z root ${@} &> stdouterr.txt"
