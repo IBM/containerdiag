546d2c7c24e0fc383d9a2c645e87720e6770a9eb
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2022-06-21 12:46:37 -0500

    Add support for deployment processing
    Signed-off-by: Kevin Grigorenko <kevin.grigorenko@us.ibm.com>

--- containerdiag.bat
+++ containerdiag.bat
@@ -8,15 +8,15 @@ set "SCRIPTNAME=%~nx0"
 goto :start
 
 :usage
-  echo usage: !SCRIPTNAME! [options] [/d DEPLOYMENT] [/p POD] COMMANDS...
+  echo usage: !SCRIPTNAME! [options] [-d DEPLOYMENT] [-p POD] COMMANDS...
   echo(
-  echo(       /d DEPLOYMENT: Run COMMANDS on all pods in the specified DEPLOYMENT
-  echo(       /i IMAGE: The image used for the debug pod (default quay.io/ibm/containerdiag)
-  echo(       /k: By default, this script uses oc if available. This options forces the use of kubectl
-  echo(       /n NAMESPACE: Namespace (optional; defaults to current namespace)
-  echo(       /p POD: Run COMMANDS on the specified POD
-  echo(       /q: Do not append the pod name to COMMANDS
-  echo(       /v: verbose output to stderr
+  echo(       -d DEPLOYMENT: Run COMMANDS on all pods in the specified DEPLOYMENT
+  echo(       -i IMAGE: The image used for the debug pod (default quay.io/ibm/containerdiag)
+  echo(       -k: By default, this script uses oc if available. This options forces the use of kubectl
+  echo(       -n NAMESPACE: Namespace (optional; defaults to current namespace)
+  echo(       -p POD: Run COMMANDS on the specified POD
+  echo(       -q: Do not append the pod name to COMMANDS
+  echo(       -v: verbose output to stderr
   echo(
   echo(       COMMANDS will be passed to oc debug node along with the pod name at the end
   echo(       (unless -q is specified in which case the pod name is not appended)
@@ -105,7 +105,7 @@ for /L %%J in (!REMAININGINDEX!,1,!I!) do (
 
 if "!TARGETDEPLOYMENT!" == "" (
   if "!TARGETPOD!" == "" (
-    echo ERROR: Either /d DEPLOYMENT or /p POD must be specified
+    echo ERROR: Either -d DEPLOYMENT or -p POD must be specified
     goto :usage
   )
 )
@@ -117,23 +117,75 @@ call :printInfo Script started with !CTL! and !IMAGE!
 if "!VERBOSE!" == "1" call :printVerbose Commands: !REMAININGARGS!
 
 if "!NAMESPACE!" == "" (
-  for /F "tokens=* USEBACKQ" %%g in (`!CTL! config view --minify --output "jsonpath={..namespace}"`) do set "NAMESPACE=%%g"
+  for /F "tokens=* USEBACKQ" %%x in (`!CTL! config view --minify --output "jsonpath={..namespace}"`) do set "NAMESPACE=%%x"
   if "!NAMESPACE!" == "" set "NAMESPACE=default"
 )
 
 if not "!TARGETDEPLOYMENT!" == "" (
   call :printInfo Querying available replicas for deployment !TARGETDEPLOYMENT! in namespace !NAMESPACE!
 
-  echo ERROR: Only -p POD is implemented
-  goto :usage
+  for /F "tokens=* USEBACKQ" %%x in (`!CTL! get deployment !TARGETDEPLOYMENT! "--namespace=!NAMESPACE!" "--output=jsonpath={.status.availableReplicas}"`) do set "AVAILABLEREPLICAS=%%x"
+  if "!AVAILABLEREPLICAS!" == "" (
+    call :printInfo Error getting deployment information. Ensure you specify the right namespace with -n NAMESPACE
+    exit /B 1
+  )
+
+  if "!AVAILABLEREPLICAS!" == "0" (
+    call :printInfo There are 0 available replicas for this deployment
+    exit /B 1
+  )
+
+  call :printInfo There are !AVAILABLEREPLICAS! available replicas
+
+  call :printInfo Querying deployment selector label
+
+  for /F "tokens=* USEBACKQ" %%x in (`!CTL! get --raw "/apis/apps/v1/namespaces/!NAMESPACE!/deployments/!TARGETDEPLOYMENT!/scale"`) do set "SELECTORRAW=%%x"
+  if "!SELECTORRAW!" == "" (
+    call :printInfo Error getting scale information. See previous output.
+    exit /B 1
+  )
+
+  set "SELECTOR=!SELECTORRAW:*selector":"=!"
+  for /f delims^=^"^ tokens^=1 %%a in ("!SELECTOR!") do set "SELECTOR=%%a"
+  if "!SELECTOR!" == "" (
+    call :printInfo Error getting scale selector from !SELECTORRAW!
+    exit /B 1
+  )
+
+  call :printInfo Selector labels are !SELECTOR!
+
+  call :printInfo Getting pods by selector
+
+  for /F "tokens=* USEBACKQ" %%x in (`!CTL! get pods --namespace !NAMESPACE! --selector !SELECTOR! --output "jsonpath={range .items[*]}{.metadata.name}{' '}{.spec.nodeName}{'\n'}{end}"`) do (
+    call :printInfo Found pod: %%x
+    set "TARGETPOD="
+    set "WORKER="
+    for /F "tokens=1,2 delims= " %%y in ("%%x") do (
+      set "TARGETPOD=%%y"
+      set "WORKER=%%z"
+    )
+
+    if "!TARGETPOD!" == "" (
+      call :printInfo Could not find pod name from %%x
+      exit /B 1
+    )
+
+    if "!WORKER!" == "" (
+      call :printInfo Could not find worker from %%x
+      exit /B 1
+    )
+
+    call :processPod !REMAININGARGS!
+  )
+
 ) else (
   if not "!TARGETPOD!" == "" (
     call :printInfo Querying worker node for pod !TARGETPOD! in namespace !NAMESPACE!
 
-    for /F "tokens=* USEBACKQ" %%g in (`!CTL! get pod !TARGETPOD! --namespace !NAMESPACE! --output "jsonpath={.spec.nodeName}"`) do set "WORKER=%%g"
+    for /F "tokens=* USEBACKQ" %%x in (`!CTL! get pod !TARGETPOD! --namespace !NAMESPACE! --output "jsonpath={.spec.nodeName}"`) do set "WORKER=%%x"
     
     if "!WORKER!" == "" (
-      call :printInfo Error getting pod information. See previous output. Ensure you specify the right namespace with /n NAMESPACE
+      call :printInfo Error getting pod information. See previous output. Ensure you specify the right namespace with -n NAMESPACE
       exit /B 1
     )
 
